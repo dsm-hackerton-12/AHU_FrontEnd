cc7abb2d99305f6760709eaac0016ae470e7397f
hoseon07 <a01081930429@gmail.com>
2025-07-14 20:17:58 +0900

    setting :: API 세팅

---
+++ src/apis/axios.ts
@@ -0,0 +1,29 @@
+import axios from "axios";
+import { Cookie } from "../utils/cookie";
+
+export const instance = axios.create({
+  baseURL: import.meta.env.VITE_BASE_URL,
+  timeout: 3000,
+});
+
+instance.interceptors.request.use(
+  (config) => {
+    const token = Cookie.get("token");
+    if (token) config.headers.Authorization = `Bearer ${token}`;
+    return config;
+  },
+  (err) => {
+    alert("오류가 발생했습니다");
+    return Promise.reject(err);
+  }
+);
+
+instance.interceptors.response.use(
+  (res) => {
+    return res;
+  },
+  (err) => {
+    console.log(err);
+    return Promise.reject(err);
+  }
+);
