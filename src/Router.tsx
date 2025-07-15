e3f27523c470b1a6db35a90f0df087b8ba07f913
abc100 <abccba111307@gmail.com>
2025-07-16 08:47:17 +0900

    feat :: 단어 등록 컴포넌트 개발

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,7 +1,11 @@
+import {BrowserRouter, Routes, Route} from "react-router-dom"
+import TopTextLayer from "./components/topTextLayer/TopTextLayer"
+import RecommendKeywrod from "./components/recommendKeyword/RecommedKeyword"
 import styled from "@emotion/styled"
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Home from "./pages/Home"
 import Aside from "./components/Aside/Index"
+import WordInfo from "./components/wordInfo/WordInfo"
 import Search from "./pages/Search"
 
 const Body = styled.div`
@@ -13,6 +17,12 @@ const Body = styled.div`
 export default function Router() {
   return (
     <BrowserRouter>
+      <Routes>
+        <Route path="/t" element={<TopTextLayer title="이선우" text="공부해" />} />
+        <Route path="/r" element={<RecommendKeywrod />}></Route>
+        <Route path="/" element={<Aside />} />
+        <Route path="/w" element={<WordInfo />}></Route>
+      </Routes>
       <Body>
         <Aside />
         <Routes>
