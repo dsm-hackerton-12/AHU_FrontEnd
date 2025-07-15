0ab7c5fd49ca8e6203f312a2124f1d85507ca5fc
Sepro <shtmdwns0@gmail.com>
2025-07-15 20:27:22 +0900

    feat :: 검색바 추가

--- src/components/recommendKeyword/RecommedKeyword.tsx
+++ src/components/recommendKeyword/RecommedKeyword.tsx
@@ -19,40 +19,42 @@ function RecommendKeyword() {
 }
 
 const RecommendKeywordContainer = styled.div`
-  width: 1268px;
+  width: 60vw;
   border: 3px solid #D8ADFF;
   border-radius: 30px;
+  padding: 2vw
 `
+
 const GraphImg = styled.img`
   width: 29px;
   height: 26px;
-  padding: 25px 0 0 31px;
 `
+
 const RecommendTitle = styled.h2`
   display: inline-block;
-  font-size: 40px;
+  font-size: 32px;
   font-weight: 600px;
   color: #8338E9;
   margin: 0;
-  padding: 19px 0 0 27px;
+  padding-left: 15px;
 `
+
 const RecommendKeywordBox = styled.div`
   display: flex;
-  padding: 9px 49px 26px;
-  gap: 27px;
+  gap: 20px;
+  padding: 10px 0 0 20px;
 `
+
 const Keyword = styled.span`
-  font-size: 25px;
+  font-size: 20px;
   font-weight: 500;
   box-sizing: border-box;
   text-align: center;
-  width: 130px;
-  padding: 10px 0;
+  width: 8vw;
+  padding: 6px;
   background-color: #F1E8FF;
   color: #9334E9;
   border-radius: 20px;
 `
-//figma랑 값을 똑같이 줬는데 너무 달라요
-//뭐가 문제인지 모르겠어요
 
 export default RecommendKeyword;
