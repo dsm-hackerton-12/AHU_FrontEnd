14cd7760bd7a8eb53c093911eff6e9b68a90022f
abc100 <abccba111307@gmail.com>
2025-07-16 10:09:47 +0900

    feat :: 라우팅 기능 개발

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,32 +1,21 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
-import TopTextLayer from "./components/topTextLayer/TopTextLayer";
 import AsideLayout from "./components/Layout/AsideLayout";
 import Profile from "./pages/MyPage";
-import BarGraph from "./components/Graph/Index";
 import Vote from "./pages/Vote";
-
-const Body = styled.div`
-  display: flex;
-  gap: 9vw;
-  height: 100vh;
-`
+import Home from "./pages/home";
+import Search from "./pages/Search";
 
 export default function Router() {
   return (
     <BrowserRouter>
       <Routes>
         <Route element={<AsideLayout />} >
+          <Route path="/" element={<Home />} />
           <Route path="/My" element={<Profile />} />
           <Route path="/vote" element={<Vote />} />
+          <Route path="/search" element={<Search />} />
         </Route>
       </Routes>
-      <Body>
-        <Aside />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/search" element={<Search />} />
-        </Routes>
-      </Body>
     </BrowserRouter>
   )
 }
--- src/components/Aside/Index.tsx
+++ src/components/Aside/Index.tsx
@@ -5,9 +5,15 @@ import Logo from "../Logo/Index";
 import { asideIcons } from "./asideIcons";
 import Profile from "./Profile";
 import Logout from "./LogOut";
+import { useNavigate } from "react-router-dom";
 
 export default function Aside() {
   const [selected, setSelected] = useState<string>("home");
+  const navigate = useNavigate();
+
+  const handleMove = (path: string) => {
+    navigate(path)
+  }
 
   return (
     <S.Container>
@@ -15,11 +21,15 @@ export default function Aside() {
       <S.MenuWrapper>
         {asideIcons.map((btn) => {
           const isSelected = selected === btn.key;
+          const path = btn.path;
           return (
             <S.Btn
               key={btn.id}
               isSelected={isSelected}
-              onClick={() => setSelected(btn.key)}
+              onClick={() => {
+                setSelected(btn.key)
+                handleMove(path)
+              }}
             >
               <img src={isSelected ? btn.clickedIcon : btn.icon} alt={`${btn.label} 아이콘`} />
               <p>{btn.label}</p>
