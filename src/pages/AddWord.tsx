1764250237e2833525e55aa0ce4598ac615b25c1
abc100 <abccba111307@gmail.com>
2025-07-16 16:03:07 +0900

    chore :: Refectoring

--- src/pages/AddWord.tsx
+++ src/pages/AddWord.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import styled from "@emotion/styled"
 import WordInfo from "../components/wordInfo/WordInfo";
 import TopTextLayer from "../components/topTextLayer/TopTextLayer";
